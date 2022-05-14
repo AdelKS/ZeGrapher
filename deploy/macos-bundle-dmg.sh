c3d697a7396cb1b68db0216e23acce195af885c1
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2022-05-15 01:03:11 +0200

    CI: Macos: add files for proper bundling
    Signed-off-by: Adel KARA SLIMANE <adel.ks@zegrapher.com>

--- deploy/macos-bundle-dmg.sh
+++ deploy/macos-bundle-dmg.sh
@@ -3,10 +3,10 @@
 deploy_dir=$(greadlink -f $(dirname "$BASH_SOURCE"))
 [[ -f "${deploy_dir}/build-macos" ]] && rm -r "${deploy_dir}/build-macos"
 
-meson setup "${deploy_dir}/build-macos" "${deploy_dir}/.." -Dprefix="${deploy_dir}/appdir-macos/usr"
+meson setup "${deploy_dir}/build-macos" "${deploy_dir}/.." --prefix="${deploy_dir}/ZeGrapher.app" --bindir="Contents/MacOS"
 cd "${deploy_dir}/build-macos"
 meson compile
 meson install
 cd ..
 
-macdeployqt build-macos/src/ZeGrapher.app -dmg
+macdeployqt ZeGrapher.app -dmg
