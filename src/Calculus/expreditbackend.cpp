86b31b7bc6a3a2574c22c65287865908ecee78d4
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2024-07-13 22:00:50 +0200

    move zcErrorToStr() to separate header
    Signed-off-by: Adel KARA SLIMANE <adel.ks@zegrapher.com>

--- src/Calculus/expreditbackend.cpp
+++ src/Calculus/expreditbackend.cpp
@@ -1,67 +1,8 @@
 #include "expreditbackend.h"
+#include "Utils/zc.h"
 #include "information.h"
 
 #include <zecalculator/zecalculator.h>
-
-QString zcErrorToStr(const zc::Error& err)
-{
-  switch (err.type)
-  {
-    case zc::Error::CPP_INCORRECT_ARGNUM:
-      return QObject::tr("Programmatically calling C++ math function with wrong number of arguments");
-
-    case zc::Error::NOT_MATH_OBJECT_DEFINITION:
-      return QObject::tr("Not a correct math object definition");
-
-    case zc::Error::OBJECT_NOT_IN_WORLD:
-      return QObject::tr("Object not registered in current math world");
-
-    case zc::Error::NAME_ALREADY_TAKEN:
-      return QObject::tr("Name already taken");
-
-    case zc::Error::UNKNOWN:
-      return QObject::tr("Unkown error");
-
-    case zc::Error::WRONG_FORMAT:
-      return QObject::tr("Wrong format");
-
-    case zc::Error::UNEXPECTED:
-      return QObject::tr("Unexpected");
-
-    case zc::Error::UNEXPECTED_END_OF_EXPRESSION:
-      return QObject::tr("Unexpected end of expression");
-
-    case zc::Error::MISSING:
-      return QObject::tr("Missing");
-
-    case zc::Error::UNDEFINED_VARIABLE:
-      return QObject::tr("Undefined variable");
-
-    case zc::Error::UNDEFINED_FUNCTION:
-      return QObject::tr("Undefined function");
-
-    case zc::Error::CALLING_FUN_ARG_COUNT_MISMATCH:
-      return QObject::tr("Calling function with wrong number of arguments");
-
-    case zc::Error::NOT_IMPLEMENTED:
-      return QObject::tr("Feature not implemented");
-
-    case zc::Error::EMPTY_EXPRESSION:
-      return QObject::tr("Empty expression");
-
-    case zc::Error::RECURSION_DEPTH_OVERFLOW:
-      return QObject::tr("Maximum recursion depth has been attained");
-
-    case zc::Error::WRONG_OBJECT_TYPE:
-      return QObject::tr("Objet has the wrong type");
-
-    case zc::Error::OBJECT_INVALID_STATE:
-      return QObject::tr("Objet in invalid state");
-
-    default:
-      return QObject::tr("Unsupported error code");
-  }
-}
 
 void ExprEditBackend::highlightBlock(const QString &text)
 {
@@ -76,9 +17,9 @@ void ExprEditBackend::highlightBlock(const QString &text)
   {
     setFormat(err.token.begin, err.token.substr.size(), invalidFormat);
 
-    if (errorMsg != zcErrorToStr(err))
+    if (errorMsg != zg::zcErrorToStr(err))
     {
-      errorMsg = zcErrorToStr(err);
+      errorMsg = zg::zcErrorToStr(err);
       emit errorMsgChanged(errorMsg);
     }
 
