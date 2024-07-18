945463766312f34da0f07eb33ac6a4690f6c850b
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2024-07-18 22:29:51 +0200

    src/print-dev-files.sh: add sorting for reproducible output
    Signed-off-by: Adel KARA SLIMANE <adel.ks@zegrapher.com>

--- src/print-dev-files.sh
+++ src/print-dev-files.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
+# outputs the source files that meson should handle
 echo "ui_files = files("
-for f in $(find . -type f -name "*.ui"); do
+for f in $(find . -type f -name "*.ui" | sort); do
     echo "  '$f',"
 done
 echo ")"
 echo
 
 echo "source_files = files("
-for f in $(find . -type f -name "*.cpp"); do
+for f in $(find . -type f -name "*.cpp" -and -not -name "main.cpp" | sort); do
     echo "  '$f',"
 done
 echo ")"
 echo
 
 echo "moc_header_files = files("
-for f in $(grep -E -lir --include=*.h "(Q_OBJECT|Q_GADGET)" .); do
+for f in $(grep -E -lir --include=*.h "(Q_OBJECT|Q_GADGET)" . | sort); do
     echo "  '$f',"
 done
 echo ")"
