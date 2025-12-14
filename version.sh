5519aa4a0f0b5d259995d5dfbe5388bb60abe41f
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2025-12-14 12:08:56 +0100

    meson & ci: unify versioning string

--- version.sh
+++ version.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 if [ "$1" = "get-vcs" ]; then
-  git -C "$MESON_SOURCE_ROOT" describe --tags
+  latest_tag=$(git -C "$MESON_SOURCE_ROOT" tag --list --sort=-creatordate | grep -v continuous | head -n1)
+  rev_count=$(git rev-list --count "${latest_tag}"..HEAD)
+  echo "$latest_tag-r$rev_count"
 elif [ "$1" = "set-dist" ]; then
   $MESONREWRITE --sourcedir="$MESON_PROJECT_DIST_ROOT" kwargs set project / version "$2"
 else
