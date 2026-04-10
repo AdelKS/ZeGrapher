ccfd2cc707b8ed920da46b1a624702ac617ccce5
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2026-04-10 20:41:32 +0200

    C++: AppSettings: make correct syntax color ZeGrapher's color

--- src/Utils/appsettings.h
+++ src/Utils/appsettings.h
@@ -49,7 +49,7 @@ public:
   QLocale::Language language;
   QFont font;
 
-  ThemedColor validSyntax = {.dark = Qt::darkGreen, .light = Qt::darkGreen};
+  ThemedColor validSyntax = {.dark = "#009999", .light = "#00fefe"};
   ThemedColor invalidSyntax = {.dark = Qt::darkRed, .light = Qt::darkRed};
   ThemedColor warningSyntax = {.dark = Qt::darkYellow, .light = Qt::darkYellow};
 
