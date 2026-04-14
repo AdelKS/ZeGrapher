c339242a5466a74dff4082b1794b5d9fc0d89a6a
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2026-04-15 01:39:45 +0200

    C++: save pixel-unit points too
    Avoids computing that many many times

--- src/Utils/sampledcurve.impl.h
+++ src/Utils/sampledcurve.impl.h
@@ -9,6 +9,7 @@ void SampledCurve<t>::clear()
 {
   input.clear();
   curve.clear();
+  px_curve.clear();
 
   if constexpr (t == CurveType::CONTINUOUS)
     discontinuities.clear();
@@ -36,6 +37,10 @@ void SampledCurve<t>::pop_front(size_t pop_num)
 template <CurveType t>
 void SampledCurve<t>::erase_chunk(size_t from, size_t size)
 {
+  assert(input.size() == curve.size());
+  assert(px_curve.size() == input.size());
+  assert(from + size <= curve.size());
+
   if (size == 0)
     return;
 
@@ -43,6 +48,7 @@ void SampledCurve<t>::erase_chunk(size_t from, size_t size)
 
   input.erase(input.begin() + from, input.begin() + to);
   curve.erase(curve.begin() + from, curve.begin() + to);
+  px_curve.erase(px_curve.begin() + from, px_curve.begin() + to);
 }
 
 template <CurveType t>
@@ -62,28 +68,40 @@ zg::real_unit SampledCurve<t>::get_smallest_allowed_step() const
 };
 
 template <CurveType t>
-void SampledCurve<t>::insert_chunk(size_t index, const std::vector<real_unit>& x, const std::vector<real_pt>& f_x)
+void SampledCurve<t>::insert_chunk(size_t index,
+                                   const std::vector<real_unit>& x,
+                                   const std::vector<real_pt>& f_x,
+                                   const std::vector<QPointF>& px_f_x)
 {
   assert(x.size() == f_x.size());
 
   input.insert(input.begin() + index, x.begin(), x.end());
   curve.insert(curve.begin() + index, f_x.begin(), f_x.end());
+  px_curve.insert(px_curve.begin() + index, px_f_x.begin(), px_f_x.end());
   if constexpr (t == CurveType::CONTINUOUS)
     discontinuities.clear();
 }
 
 template <CurveType t>
-void SampledCurve<t>::sparse_insert(std::vector<size_t> indices, std::vector<real_unit> x, std::vector<real_pt> f_x)
+void SampledCurve<t>::sparse_insert(const std::vector<size_t>& indices,
+                                    const std::vector<real_unit>& x,
+                                    const std::vector<real_pt>& f_x,
+                                    const std::vector<QPointF>& px_f_x)
 {
   assert(indices.size() <= curve.size()); // can only insert as many points as there are originally in  'curve'
   assert(std::ranges::adjacent_find(indices, std::greater_equal{}) == indices.end()); // strictly increasing (implies sorted + unique)
-  assert(indices.size() == x.size() and x.size() == f_x.size());
+  assert(indices.size() == x.size() and x.size() == f_x.size() and f_x.size() == px_f_x.size());
 
-  std::vector<real_pt> new_curve;
-  new_curve.reserve(curve.size() + indices.size());
+  const size_t new_size = curve.size() + indices.size();
 
   std::vector<real_unit> new_input;
-  new_input.reserve(curve.size() + indices.size());
+  new_input.reserve(new_size);
+
+  std::vector<real_pt> new_curve;
+  new_curve.reserve(new_size);
+
+  std::vector<QPointF> new_px_curve;
+  new_px_curve.reserve(new_size);
 
   size_t curve_index = 0, indices_metaindex = 0;
   while (curve_index != curve.size() and indices_metaindex != indices.size())
@@ -92,21 +110,25 @@ void SampledCurve<t>::sparse_insert(std::vector<size_t> indices, std::vector<rea
     {
       new_input.push_back(x[indices_metaindex]);
       new_curve.push_back(f_x[indices_metaindex]);
+      new_px_curve.push_back(px_f_x[indices_metaindex]);
       indices_metaindex++;
     }
     else
     {
       new_input.push_back(input[curve_index]);
       new_curve.push_back(curve[curve_index]);
+      new_px_curve.push_back(px_curve[curve_index]);
       curve_index++;
     }
   }
 
   new_input.insert(new_input.end(), input.begin() + curve_index, input.end());
   new_curve.insert(new_curve.end(), curve.begin() + curve_index, curve.end());
+  new_px_curve.insert(new_px_curve.end(), px_curve.begin() + curve_index, px_curve.end());
 
-  curve = std::move(new_curve);
   input = std::move(new_input);
+  curve = std::move(new_curve);
+  px_curve = std::move(new_px_curve);
 }
 
 } // namespace zg
