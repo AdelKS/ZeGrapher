0d2bbc7abd926808c4887cfa67a045bf36408d37
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2026-04-26 00:47:41 +0200

    QML-C++: MathWorld: add revdep methods

--- src/MathObjects/mathworld.cpp
+++ src/MathObjects/mathworld.cpp
@@ -94,4 +94,80 @@ QHash<int, QByteArray> MathWorld::roleNames() const
   return { {Qt::DisplayRole, "mathObjPtr"} };
 }
 
+std::unordered_set<MathObject*> MathWorld::direct_revdeps(MathObject& c) const
+{
+  using PairT = std::pair<zg::MathObject*, PlotStyle*>;
+
+  assert (std::ranges::find(mathObjects, &c, &PairT::first) != mathObjects.end() or
+          std::ranges::find(altMathObjects, &c) != altMathObjects.end());
+
+  std::unordered_set<std::string> direct_revdep_names;
+
+  std::visit(
+    zc::utils::overloaded{
+      [&](const zc::DynMathObject<zc_t>* p){
+        for (auto&& [name, t]: zc::mathWorld.direct_revdeps(p->get_name())) direct_revdep_names.insert(name);
+      },
+      [&](std::pair<const zc::DynMathObject<zc_t>*, const zc::DynMathObject<zc_t>*> p) {
+        for (auto&& [name, _]: zc::mathWorld.direct_revdeps(p.first->get_name())) direct_revdep_names.insert(name);
+        for (auto&& [name, _]: zc::mathWorld.direct_revdeps(p.second->get_name())) direct_revdep_names.insert(name);
+      },
+      [](std::monostate){},
+    },
+    c.getZcObject()
+  );
+
+  std::unordered_set<const zc::DynMathObject<zc_t>*> direct_revdep_backends;
+
+  for (zc::DynMathObject<zc_t>& b: zc::mathWorld)
+    if (auto it = direct_revdep_names.find(std::string(b.get_name())); it != direct_revdep_names.end())
+      direct_revdep_backends.insert(&b);
+
+  std::unordered_set<MathObject*> direct_revdeps_res;
+
+  auto insert_revdep = [&](MathObject* r){
+    std::visit(
+      zc::utils::overloaded{
+        [&](const zc::DynMathObject<zc_t>* p){
+          if (direct_revdep_backends.contains(p)) direct_revdeps_res.insert(r);
+        },
+        [&](std::pair<const zc::DynMathObject<zc_t>*, const zc::DynMathObject<zc_t>*> p) {
+          if (direct_revdep_backends.contains(p.first)) direct_revdeps_res.insert(r);
+          else if (direct_revdep_backends.contains(p.second)) direct_revdeps_res.insert(r);
+        },
+        [](std::monostate){},
+      },
+      r->getZcObject()
+    );
+  };
+
+  for (auto&& [r, _]: mathObjects)
+    insert_revdep(r);
+
+  for (MathObject* r: altMathObjects)
+    insert_revdep(r);
+
+  return direct_revdeps_res;
+}
+
+std::unordered_set<MathObject*> MathWorld::revdeps(MathObject& c) const
+{
+  std::unordered_set<MathObject*> to_explore = direct_revdeps(c), revdeps_res, explored;
+
+  while (not to_explore.empty())
+  {
+    MathObject* d = *to_explore.begin();
+    to_explore.erase(to_explore.begin());
+
+    explored.insert(d);
+    revdeps_res.insert(d);
+    std::unordered_set<MathObject*> new_to_explore = direct_revdeps(*d);
+    for (MathObject* e: new_to_explore)
+      if (not explored.contains(e))
+        to_explore.insert(e);
+  }
+
+  return revdeps_res;
+}
+
 }
