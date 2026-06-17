32cae7377e4b6b7a85cfa0fc9aa418cb82c63bd3
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2026-06-17 20:05:42 +0300

    scripts: version.sh: increment last number of latest available tag
    if it's not the same commit

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e
 
@@ -9,7 +9,15 @@ fi
 if [ "$1" = "get-vcs" ]; then
   latest_tag=$(git -C "$MESON_SOURCE_ROOT" tag --list --sort=-creatordate | grep -v continuous | head -n1)
   rev_count=$(git -C "$MESON_SOURCE_ROOT" rev-list --count "${latest_tag}"..HEAD)
-  echo "$latest_tag-r$rev_count"
+  if [[ $latest_tag =~ ^(.*)([0-9]+)$ ]]; then
+    increment=1
+    [[ "$rev_count" == "0" ]] && increment=0
+    digits="${BASH_REMATCH[2]}"
+    echo "${BASH_REMATCH[1]}$(( digits + increment ))"
+  else
+    echo "Tag $latest_tag has unsupported format"
+    exit 1
+  fi
 elif [ "$1" = "set-dist" ]; then
   $MESONREWRITE --sourcedir="$MESON_PROJECT_DIST_ROOT" kwargs set project / version "$2"
 else
