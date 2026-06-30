AdelKS/ZeGrapher

commit f46a4cb2a743cd6cc5853899d16edf8d47834740

C++: Graph: better handle 0 coordinate
- Was coming sometimes as e.g. 1.12345E-17 which takes up space to
  write and the margin was accounting for it
- Improve logic when to draw axes: no need to loop through the ticks
  - drop 0 from the calculated ticks altogether

src/GraphDraw/graph.h
@@ -247,8 +247,6 @@ void Graph::drawLinCoordinateTicks()
     else return std::tie(std::as_const(viewMapper.y), yAxisTicks.ticks);
   }();
 
-  const zg::pixel_unit zero_pt = axisMapper.template to<zg::pixel>(zg::real_unit{0.});
-
   for (const ZeLinAxisTick &axisTick : axisTicks)
   {
     if (not(axisMapper.template getRange<zg::real>().min < axisTick.pos
@@ -257,46 +255,32 @@ void Graph::drawLinCoordinateTicks()
 
     zg::pixel_unit px_pos = axisMapper.template to<zg::pixel>(axisTick.pos);
 
-    if (fabs(px_pos.v - zero_pt.v) > 1.)
-    {
-      drawTick<axis>(px_pos, axesSettings.color.getCurrent(), axesSettings.lineWidth);
-      writeCoordinate<axis>(px_pos, axisTick.posStr);
-    }
+    drawTick<axis>(px_pos, axesSettings.color.getCurrent(), axesSettings.lineWidth);
+    writeCoordinate<axis>(px_pos, axisTick.posStr);
+
   }
+
+  const zg::pixel_unit zero_pt = axisMapper.template to<zg::pixel>(zg::real_unit{0.});
+
+  if (axisMapper.isInView(zero_pt))
+    writeCoordinate<axis>(zero_pt, "0");
 }
 
 template <ZeAxisName axis>
 void Graph::drawLinAxis()
 {
-  painter->setFont(settings.getFont());
-  QFontMetrics fontMetrics = painter->fontMetrics();
-
   const ZeAxesSettings &axesSettings = settings.getAxes();
 
-  pen.setCapStyle(Qt::FlatCap);
-
-  const auto& [axisMapper, axisTicks] = [this]{
+  const auto& axisMapper = [this]{
     if constexpr (axis == ZeAxisName::X)
-      return std::tie(std::as_const(viewMapper.x), xAxisTicks.ticks);
-    else return std::tie(std::as_const(viewMapper.y), yAxisTicks.ticks);
+      return viewMapper.x;
+    else return viewMapper.y;
   }();
 
   const zg::pixel_unit zero_pt = axisMapper.template to<zg::pixel>(zg::real_unit{0.});
 
-  for (const ZeLinAxisTick &axisTick : axisTicks)
-  {
-    if (not(axisMapper.template getRange<zg::real>().min < axisTick.pos
-            && axisTick.pos < axisMapper.template getRange<zg::real>().max))
-      continue;
-
-    zg::pixel_unit px_pos = axisMapper.template to<zg::pixel>(axisTick.pos);
-
-    if (fabs(px_pos.v - zero_pt.v) <= 1.)
-    {
-      drawLine<axis>(zero_pt, axesSettings.color.getCurrent(), axesSettings.lineWidth);
-      writeCoordinate<axis>(zero_pt, "0");
-    }
-  }
+  if (axisMapper.isInView(zero_pt))
+    drawLine<axis>(zero_pt, axesSettings.color.getCurrent(), axesSettings.lineWidth);
 }
 
 template <ZeAxisName axis>

src/GraphDraw/gridcalculator.h
@@ -279,12 +279,15 @@ ZeLinAxisTicks GridCalculator::getLinearAxisTicks(const zg::ZeAxisMapper<axis> &
   double multiplier = (floor(scaledOffsetRange.min.v / realStep) - 1) * realStep;
   double power_offset = int_pow(10.0, axisTicks.offset.basePowerOffset);
 
-  // TODO: offset is not good, figure it out
+  const zg::pixel_unit zero_pt = axis_mapper.template to<zg::pixel>(zg::real_unit{0.});
 
   do
   {
     ZeLinAxisTick tick;
     multiplier += realStep;
+    zg::pixel_unit px_mul = axis_mapper.template to<zg::pixel>(zg::real_unit{multiplier});
+    if (fabs(px_mul.v - zero_pt.v) <= 1.)
+      continue;
     tick.pos = {multiplier * constantMultiplier * power_offset + axisTicks.offset.sumOffset};
     tick.posStr = get_coordinate_string(axisSettings, multiplier);
 

commit 7d2a0d58bc1a8dc6fe2723b86f24564dfe8c250a

C++: fix subgrid calculation around 0
Bug was introduced by f46a4cb: stopped adding 0 to the tick list
but the subgrid relied on that, add it back (and skip it in the
relevant code path).

src/GraphDraw/graph.h
@@ -247,6 +247,9 @@ void Graph::drawLinCoordinateTicks()
     else return std::tie(std::as_const(viewMapper.y), yAxisTicks.ticks);
   }();
 
+
+  const zg::pixel_unit zero_pt = axisMapper.template to<zg::pixel>(zg::real_unit{0.});
+
   for (const ZeLinAxisTick &axisTick : axisTicks)
   {
     if (not(axisMapper.template getRange<zg::real>().min < axisTick.pos
@@ -255,15 +258,11 @@ void Graph::drawLinCoordinateTicks()
 
     zg::pixel_unit px_pos = axisMapper.template to<zg::pixel>(axisTick.pos);
 
-    drawTick<axis>(px_pos, axesSettings.color.getCurrent(), axesSettings.lineWidth);
+    if (fabs(px_pos.v - zero_pt.v) > 1.)
+      drawTick<axis>(px_pos, axesSettings.color.getCurrent(), axesSettings.lineWidth);
+
     writeCoordinate<axis>(px_pos, axisTick.posStr);
-
   }
-
-  const zg::pixel_unit zero_pt = axisMapper.template to<zg::pixel>(zg::real_unit{0.});
-
-  if (axisMapper.isInView(zero_pt))
-    writeCoordinate<axis>(zero_pt, "0");
 }
 
 template <ZeAxisName axis>

src/GraphDraw/gridcalculator.h
@@ -287,7 +287,7 @@ ZeLinAxisTicks GridCalculator::getLinearAxisTicks(const zg::ZeAxisMapper<axis> &
     multiplier += realStep;
     zg::pixel_unit px_mul = axis_mapper.template to<zg::pixel>(zg::real_unit{multiplier});
     if (fabs(px_mul.v - zero_pt.v) <= 1.)
-      continue;
+      multiplier = 0;
     tick.pos = {multiplier * constantMultiplier * power_offset + axisTicks.offset.sumOffset};
     tick.posStr = get_coordinate_string(axisSettings, multiplier);
 