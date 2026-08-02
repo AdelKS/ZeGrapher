f23d05e0d6923bc32511e6c1b4112c3fe2ce2f67
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2026-08-02 15:59:44 +0200

    C++: DataTableModel: add rowCountWithout()
    Will be used for code factorization

--- src/DataPlot/datatablemodel.h
+++ src/DataPlot/datatablemodel.h
@@ -71,6 +71,9 @@ signals:
 protected:
   static QString headerName(const zg::mathobj::Data*);
 
+  /// @brief rowCount without taking 'without' into account
+  int rowCountWithout(zg::mathobj::Data* without = nullptr) const;
+
   /// @brief re-reads the header of an already registered column
   void refreshColumnName(const zg::mathobj::Data*);
 
