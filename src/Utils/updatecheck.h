d04e9af2bb97c3c6c3b33a10e6ac05280e81c22f
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2026-08-28 05:03:00 +0200

    QML: add the About panel

--- src/Utils/updatecheck.h
+++ src/Utils/updatecheck.h
@@ -12,7 +12,6 @@ class UpdateCheck : public QObject
   QML_ELEMENT
 
   Q_PROPERTY(Status status MEMBER status NOTIFY statusChanged)
-  Q_PROPERTY(QString currentVersion READ getCurrentVersion)
   Q_PROPERTY(QString latestVersion READ getLatestVersion)
 
 public:
@@ -21,7 +20,6 @@ public:
   enum Status {IDLE, CHECKING, ERROR, UPDATE_MAYBE_AVAILABLE, UPDATE_AVAILABLE, UP_TO_DATE};
   Q_ENUM(Status);
 
-  Q_INVOKABLE QString getCurrentVersion() const { return currentVersion; }
   Q_INVOKABLE QString getLatestVersion() const { return latestVersion; }
 
 public slots:
